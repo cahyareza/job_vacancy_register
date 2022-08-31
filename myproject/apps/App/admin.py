85b16ffe0c4647c133524046a90832afb1c9eae4
cahyafaisalreza <cf.reza07@gmail.com>
2022-09-01 04:43:57 +0700

    Add django form

---
+++ myproject/apps/App/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Candidate
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ['firstname', 'lastname', 'email']
+    search_fields = ['firstname', 'lastname', 'email']
+    list_per_page = 10
+
+admin.site.register(Candidate, CandidateAdmin)
--- myproject/urls.py
+++ myproject/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 
 urlpatterns = i18n_patterns(
-    path('', TemplateView.as_view(template_name="index.html")),
+    path('', include('myproject.apps.App.urls')),
     path('admin/', admin.site.urls),
 )
