00a58ed425e7afcc529beba5b082e94821b2d568
cahyafaisalreza <cf.reza07@gmail.com>
2022-09-02 05:49:14 +0700

    Part 7
    Add django cleanup

--- myproject/settings/_base.py
+++ myproject/settings/_base.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third-party
+    'django_cleanup.apps.CleanupConfig',
     # local
     'myproject.apps.App'
 ]
